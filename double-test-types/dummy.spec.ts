99f7a5dd7692e9e1b51060d1727824aa627220fa
Ghosteye <yqcyecong@163.com>
2023-08-25 00:53:57 +0800

    feat: Update test types

--- double-test-types/dummy.spec.ts
+++ double-test-types/dummy.spec.ts
@@ -6,11 +6,14 @@ test("EmailService", () => {
     subject: "heihei",
     body: "hahaha",
   };
-  //   const dummyRecipient: Recipient = {
-  //     email: "",
-  //     name: "",
-  //   };
-  //   const dummyRecipient = {} as Recipient;
-
-  sendEmail(message, {});
+  // 创建一个基础数据结构, 占位
+  // const dummyRecipient: Recipient = {
+  //   email: "",
+  //   name: "",
+  // };
+  // 创建一个空对象, 占位
+  const dummyRecipient = {} as Recipient;
+  sendEmail(message, dummyRecipient);
+  // 直接使用对象字面量和断言
+  sendEmail(message, {} as Recipient);
 });
--- double-test-types/stub.ts
+++ double-test-types/stub.ts
@@ -1,6 +1,6 @@
 import { getUserEmail } from "./stub.database";
 // user.js
-export function sendWelcomeEmail(userId) {
+export function sendWelcomeEmail(userId: number) {
   const email = getUserEmail(userId);
   // Send email to the user...
 
