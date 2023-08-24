16ceade361bc4eb4161f2cedd6b17ecb833fcda6
Ghosteye <yqcyecong@163.com>
2023-08-24 17:38:45 +0800

    feat: Double test types

---
+++ double-test-types/dummy.spec.ts
@@ -0,0 +1,16 @@
+import { test } from "vitest";
+import { Message, Recipient, sendEmail } from "./dummy";
+
+test("EmailService", () => {
+  const message: Message = {
+    subject: "heihei",
+    body: "hahaha",
+  };
+  //   const dummyRecipient: Recipient = {
+  //     email: "",
+  //     name: "",
+  //   };
+  //   const dummyRecipient = {} as Recipient;
+
+  sendEmail(message, {});
+});
